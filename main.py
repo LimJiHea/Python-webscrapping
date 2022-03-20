fee7b05582e3365407ce37a654ac67627e5af733
LimJiHae <91642959+LimJiHea@users.noreply.github.com>
2022-03-20 13:51:22 +0000

    commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 
 indeed_soup = BeautifulSoup(indeed_result.text,"html.parser")
